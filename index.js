3d3fe6ed1ec20744bdb008b45b404176596b0458
Vladana Ignjatic <vladana.ignjatic@develabs.net>
2023-11-16 15:06:49 +0100

    Add validators for the db scheme (3.19-3.21)

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/api/persons', (request, response) => {
 		})
 	})
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
 
 	const currTime = new Date();
 
@@ -43,7 +43,7 @@ app.get('/info', (request, response) => {
 
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
 
 	Person.findById(request.params.id)
 		.then(person => {
@@ -61,7 +61,7 @@ app.get('/api/persons/:id', (request, response) => {
 
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
 
 	Person.findByIdAndDelete(request.params.id)
 		.then(() => {
@@ -71,21 +71,9 @@ app.delete('/api/persons/:id', (request, response) => {
 
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 
 	const person = request.body
-
-	if (!person.name || !person.number) {
-		return response.status(400).json({
-			error: `persons ${!person.name ? 'name' : 'number'} missing`
-		})
-	}
-
-	/*if (persons.some(p => p.name === person.name)) {
-		return response.status(400).json({
-			error: 'name must be unique'
-		})
-	}*/
 
 	const newPerson = new Person({
 		name: person.name,
@@ -96,17 +84,15 @@ app.post('/api/persons', (request, response) => {
 		.then(savedPerson => {
 			response.json(savedPerson)
 		})
+		.catch(error => next(error))
 
 })
 
 app.put('/api/persons/:id', (request, response) => {
 
-	const person = {
-		name: request.body.name,
-		number: request.body.number
-	}
+	const { name, number } = request.body
 
-	Person.findByIdAndUpdate(request.params.id, person, {new: true})
+	Person.findByIdAndUpdate(request.params.id, { name, number}, { new: true, runValidators: true, context: 'query' })
 		.then(updatedPerson => {
 			response.json(updatedPerson)
 		})
@@ -128,6 +114,8 @@ const errorHandler = (error, request, response, next) => {
 
 	if (error.name === 'CastError') {
 		return response.status(400).send({ error: 'malformatted id' })
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
 	}
 
 	next(error)
--- models/person.js
+++ models/person.js
@@ -18,8 +18,22 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-	name: String,
-	number: String,
+	name: {
+		type: String,
+		minLength: 3,
+		required: [true, 'User name is required'],
+		unique: true
+	},
+	number: {
+		type: String,
+		validate: {
+			validator: function(v) {
+				return /^\d{2,3}-\d{3,}$/.test(v);
+			},
+			message: props => `${props.value} is not a valid phone number!`
+		},
+		required: [true, 'User phone number is required']
+	},
 })
 
 personSchema.set('toJSON', {
